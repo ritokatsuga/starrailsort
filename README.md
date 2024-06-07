2413c9c792ca0da19a23b58f23b126338188fb0d
ritokatsuga <salman.ufal@gmail.com>
2024-06-07 08:42:11 +0700

    Adding new playable and upcoming characters since Black Swan was released, new address

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
 # StarRailSort
-A Honkai: Star Rail sorter with classic layout and more filters that based on [execfera's Touhou Project character sorter](https://tohosort.frelia.my/) source code and [better-nijisort's version of Nijisanji liver sorter](https://better-nijisort/pages.dev).
+A Honkai: Star Rail sorter with classic layout and more filters that based on [execfera's Touhou Project character sorter](https://tohosort.frelia.my/) source code.
 
-Visit [starrail.sorter.my.id](https://starrail.sorter.my.id/)
-
-Latest changelog:<br>
-9 December 2023 - Separating Trailblazer, adding new options, fixing database for Indonesian version
-
-Contributors are open
+Visit [starrail.sorter.my.id](https://starrail.sorter.ufal.my.id/)
 
 Supports me via [Trakteer (Indonesia)](https://trakteer.id/ufalsalman/tip) or [PayPal (International)](https://paypal.me/ufalsalman)
 
 ## Bahasa Indonesia
 
-Sebuah penyortir termutakhirkan untuk karakter Honkai: Star Rail dengan tata letak klasik dan  filter yang lebih banyak yang berdasarkan pada kode sumber [penyortir karakter Touhou Project milik execfera](https://tohosort.frelia.my/) dan [penyortir liver Nijisanji versi better-nijisort](https://better-nijisort/pages.dev).
+Sebuah penyortir termutakhirkan untuk karakter Honkai: Star Rail dengan tata letak klasik dan  filter yang lebih banyak yang berdasarkan pada kode sumber [penyortir karakter Touhou Project milik execfera](https://tohosort.frelia.my/).
 
-Kunjungi [starrail.sorter.my.id/id](https://starrail.sorter.my.id/id)
-
-Perubahan terakhir:<br>
-9 Desember 2023 - Memisahkan Trailblazer, menambahkan opsi baru, memperbaiki database untuk versi bahasa Indonesia
-
-Menerima kontributor.
+Kunjungi [starrail.sorter.my.id/id](https://starrail.sorter.ufal.my.id/id)
 
 Dukung saya melalui [Trakteer (Indonesia)](https://trakteer.id/ufalsalman/tip) atau [PayPal (Internasional)](https://paypal.me/ufalsalman)
---
+++ src/js/data-id/2024-06-07.js
@@ -0,0 +1,743 @@
+dataSetVersion = "2024-06-07"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Saring berdasarkan faksi",
+    key: "faction",
+    tooltip: "Centang ini untuk membatasi karakter dari faksi tertentu.",
+    checked: false,
+    sub: [
+      { name: "Astral Express", key: "ae" },
+      { name: "Stellaron Hunters", key: "sh" },
+      { name: "Herta Space Station", key: "hss" },
+      { name: "Belobog", key: "be" },
+      { name: "Xianzhou Loufu", key: "xl" },
+      { name: "Knights of Beauty", key: "kb" },
+      { name: "Interastral Peace Corporation", key: "ipc" },
+      { name: "Genius Society", key: "gs" },
+      { name: "Intelligentsia Guild", key: "ig" },
+      { name: "Garden of Recollection", key: "gr" },
+      { name: "Masked Fools", key: "mf" },
+      { name: "Penacony", key: "pn" },
+      { name: "Galaxy Ranger", key: "galr" }
+    ]
+  },
+  {
+    name: "Saring berdasarkan rarity",
+    key: "star",
+    tooltip: "Centang ini untuk membatasi karakter dari rarity tertentu.",
+    checked: false,
+    sub: [
+      { name: "Bintang 5", key: "5" },
+      { name: "Bintang 4", key: "4" },
+    ]
+  },
+  {
+    name: "Saring berdasarkan tipe bertarung",
+    key: "ct",
+    tooltip: "Centang ini untuk membatasi karakter dari tipe bertarung tertentu.",
+    checked: false,
+    sub: [
+      { name: "Quantum", key: "quantum" },
+      { name: "Physical", key: "physical" },
+      { name: "Wind", key: "wind" },
+      { name: "Fire", key: "fire" },
+      { name: "Ice", key: "ice" },
+      { name: "Lightning", key: "lightning" },
+      { name: "Imaginary", key: "imaginary" },
+    ]
+  },
+  {
+    name: "Saring berdasarkan Path",
+    key: "path",
+    tooltip: "Centang ini untuk membatasi karakter dari rarity Path tertentu.",
+    checked: false,
+    sub: [
+      { name: "The Destruction", key: "td" },
+      { name: "The Preservation", key: "tp" },
+      { name: "The Hunt", key: "thu" },
+      { name: "The Erudition", key: "te" },
+      { name: "The Harmony", key: "tha" },
+      { name: "The Nihility", key: "tn" },
+      { name: "The Abundance", key: "ta" },
+    ]
+  },
+  {
+    name: "Kecualikan laki-laki",
+    key: "male",
+    tooltip: "Centang ini untuk mengecualikan laki-laki.",
+    checked: false
+  },
+  {
+    name: "Kecualikan perempuan",
+    key: "female",
+    tooltip: "Centang ini untuk mengecualikan perempuan.",
+    checked: false
+  },
+  {
+    name: "Kecualikan karakter mendatang",
+    key: "upcoming",
+    tooltip: "Centang ini untuk mengecualikan karakter mendatang/masih belum dapat dimainkan.",
+    checked: true
+  },
+  {
+    name: "Kecualikan Trailblazer",
+    key: "trailblazer",
+    tooltip: "Centang ini untuk mengecualikan Trailblazer.",
+    checked: false
+  },
+  {
+    name: "Kecualikan versi alter",
+    key: "alter",
+    tooltip: "Centang ini untuk mengecualikan karakter alter dan hanya menampilkan versi orisinal.",
+    checked: false
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Stelle - The Destruction",
+    img: "stelle1.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['td'],
+      female: true,
+      trailblazer: true
+    }
+  },
+  {
+    name: "Caelus - The Destruction",
+    img: "caelus1.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['td'],
+      male: true,
+      trailblazer: true
+    }
+  },
+  {
+    name: "March 7th",
+    img: "march.png",
+    opts: {
+      faction: ['ae'],
+      star: ['4'],
+      ct: ['ice'],
+      path: ['tp'],
+      female: true
+    }
+  },
+  {
+    name: "Dan Heng",
+    img: "danheng.png",
+    opts: {
+      faction: ['ae'],
+      star: ['4'],
+      ct: ['wind'],
+      path: ['th'],
+      male: true
+    }
+  },
+  {
+    name: "Himeko",
+    img: "himeko.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['te'],
+      female: true
+    }
+  },
+  {
+    name: "Welt",
+    img: "welt.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['tn'],
+      male: true
+    }
+  },
+  {
+    name: "Stelle - The Preservation",
+    img: "stelle2.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['tp'],
+      female: true,
+      trailblazer: true,
+      thpath: true
+    }
+  },
+  {
+    name: "Caelus - The Preservation",
+    img: "caelus2.png",
+    opts: {
+      faction: ['ae'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['tp'],
+      male: true,
+      trailblazer: true,
+      thpath: true
+    }
+  },
+  {
+    name: "Kafka",
+    img: "kafka.png",
+    opts: {
+      faction: ['sh'],
+      female: true,
+    }
+  },
+  {
+    name: "Silver Wolf",
+    img: "silverwolf.png",
+    opts: {
+      faction: ['sh'],
+      star: ['5'],
+      ct: ['quantum'],
+      path: ['tn'],
+      female: true,
+    }
+  },
+  {
+    name: "Blade",
+    img: "blade.png",
+    opts: {
+      faction: ['sh'],
+      star: ['5'],
+      ct: ['wind'],
+      path: ['td'],
+      male: true,
+    }
+  },
+  {
+    name: "Arlan",
+    img: "arlan.png",
+    opts: {
+      faction: ['hss'],
+      star: ['4'],
+      ct: ['lightning'],
+      path: ['td'],
+      male: true
+    }
+  },
+  {
+    name: "Asta",
+    img: "asta.png",
+    opts: {
+      faction: ['hss'],
+      star: ['4'],
+      ct: ['fire'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Herta",
+    img: "herta.png",
+    opts: {
+      faction: ['hss'],
+      star: ['4'],
+      ct: ['ice'],
+      path: ['te'],
+      female: true
+    }
+  },
+  {
+    name: "Bronya",
+    img: "bronya.png",
+    opts: {
+      faction: ['be'],
+      star: ['5'],
+      ct: ['wind'],
+      path: ['tha'],
+      female: true
+    }
+  },
+  {
+    name: "Seele",
+    img: "seele.png",
+    opts: {
+      faction: ['be'],
+      star: ['5'],
+      ct: ['quantum'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Gepard",
+    img: "gepard.png",
+    opts: {
+      faction: ['be'],
+      star: ['5'],
+      ct: ['ice'],
+      path: ['tp'],
+      male: true
+    }
+  },
+  {
+    name: "Clara",
+    img: "clara.png",
+    opts: {
+      faction: ['be'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['td'],
+      female: true
+    }
+  },
+  {
+    name: "Serval",
+    img: "serval.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['lightning'],
+      path: ['te'],
+      female: true
+    }
+  },
+  {
+    name: "Natasha",
+    img: "natasha.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['physical'],
+      path: ['ta'],
+      female: true
+    }
+  },
+  {
+    name: "Pela",
+    img: "pela.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['ice'],
+      path: ['tn'],
+      female: true
+    }
+  },
+  {
+    name: "Sampo",
+    img: "sampo.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['wind'],
+      path: ['tn'],
+      male: true
+    }
+  },
+  {
+    name: "Hook",
+    img: "hook.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['fire'],
+      path: ['td'],
+      female: true
+    }
+  },
+  {
+    name: "Luka",
+    img: "luka.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['physical'],
+      path: ['tn'],
+      male: true,
+    }
+  },
+  {
+    name: "Bailu",
+    img: "bailu.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['lightning'],
+      path: ['ta'],
+      female: true
+    }
+  },
+  {
+    name: "Yanqing",
+    img: "yanqing.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['ice'],
+      path: ['th'],
+      male: true
+    }
+  },
+  {
+    name: "Jingyuan",
+    img: "jingyuan.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['lightning'],
+      path: ['te'],
+      male: true
+    }
+  },
+  {
+    name: "Qingque",
+    img: "qingque.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['quantum'],
+      path: ['te'],
+      female: true
+    }
+  },
+  {
+    name: "Tingyun",
+    img: "tingyun.png",
+    opts: {
+      faction: ['xtl'],
+      star: ['4'],
+      ct: ['lightning'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Sushang",
+    img: "sushang.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['physical'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Yukong",
+    img: "yukong.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['imaginary'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Luocha",
+    img: "luocha.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['ta'],
+      male: true
+    }
+  },
+  {
+    name: "Fu Xuan",
+    img: "fuxuan.png",
+    opts: {
+      faction: ['xl'],
+      ct: ['quantum'],
+      path: ['tp'],
+      female: true,
+    }
+  },
+  {
+    name: "Dan Heng - Imbibitor Lunae",
+    img: "danheng2.png",
+    opts: {
+      faction: ['ae'],
+      ct: ['imaginary'],
+      path: ['td'],
+      male: true,
+      alter: true
+    }
+  },
+  {
+    name: "Lynx",
+    img: "lynx.png",
+    opts: {
+      faction: ['be'],
+      star: ['4'],
+      ct: ['quantum'],
+      path: ['ta'],
+      female: true,
+    }
+  },
+  {
+    name: "Jingliu",
+    img: "jingliu.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['ice'],
+      path: ['td'],
+      female: true
+    }
+  },
+  {
+    name: "Topaz & Numby",
+    img: "topaz.png",
+    opts: {
+      faction: ['ipc'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Guinaifen",
+    img: "guinaifen.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['fire'],
+      path: ['tn'],
+      female: true
+    }
+  },
+  {
+    name: "Huohuo",
+    img: "huohuo.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['wind'],
+      path: ['ta'],
+      female: true
+    }
+  },
+  {
+    name: "Argenti",
+    img: "argenti.png",
+    opts: {
+      faction: ['kb'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['te'],
+      male: true
+    }
+  },
+  {
+    name: "Hanya",
+    img: "hanya.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['physical'],
+      path: ['tha'],
+      female: true
+    }
+  },
+  {
+    name: "Ruan Mei",
+    img: "ruanmei.png",
+    opts: {
+      faction: ['gs'],
+      star: ['5'],
+      ct: ['ice'],
+      path: ['tha'],
+      female: true
+    }
+  },
+  {
+    name: "Dr. Ratio",
+    img: "ratio.png",
+    opts: {
+      faction: ['ig'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['th'],
+      male: true
+    }
+  },
+  {
+    name: "Xueyi",
+    img: "xueyi.png",
+    opts: {
+      faction: ['xl'],
+      star: ['4'],
+      ct: ['quantum'],
+      path: ['td'],
+      female: true
+    }
+  },
+  {
+    name: "Black Swan",
+    img: "blackswan.png",
+    opts: {
+      faction: ['gr'],
+      star: ['5'],
+      ct: ['wind'],
+      path: ['tn'],
+      female: true
+    }
+  },
+  {
+    name: "Sparkle",
+    img: "sparkle.png",
+    opts: {
+      faction: ['mf'],
+      star: ['5'],
+      ct: ['quantum'],
+      path: ['tha'],
+      female: true
+    }
+  },
+  {
+    name: "Misha",
+    img: "misha.png",
+    opts: {
+      faction: ['pn'],
+      star: ['4'],
+      ct: ['ice'],
+      path: ['td'],
+      male: true
+    }
+  },
+  {
+    name: "Acheron",
+    img: "acheron.png",
+    opts: {
+      faction: ['galr'],
+      star: ['5'],
+      ct: ['lightning'],
+      path: ['tn'],
+      female: true
+    }
+  },
+  {
+    name: "Aventurine",
+    img: "aventurine.png",
+    opts: {
+      faction: ['ipc'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['tp'],
+      male: true
+    }
+  },
+  {
+    name: "Gallagher",
+    img: "gallagher.png",
+    opts: {
+      faction: ['pn'],
+      star: ['4'],
+      ct: ['fire'],
+      path: ['ta'],
+      male: true
+    }
+  },
+  {
+    name: "Robin",
+    img: "robin.png",
+    opts: {
+      faction: ['pn'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['tha'],
+      female: true
+    }
+  },
+  {
+    name: "Boothill",
+    img: "boothill.png",
+    opts: {
+      faction: ['galr'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['th'],
+      male: true
+    }
+  },
+  {
+    name: "Firefly",
+    img: "firefly.png",
+    opts: {
+      faction: ['sh'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['td'],
+      female: true,
+      upcoming: true
+    }
+  },
+  {
+    name: "jade",
+    img: "jade.png",
+    opts: {
+      faction: ['ipc'],
+      star: ['5'],
+      ct: ['quantum'],
+      path: ['te'],
+      female: true,
+      upcoming: true
+    }
+  },
+  {
+    name: "Stelle - The Harmony",
+    img: "stelle3.png",
+    opts: {
+      faction: ['ast'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['th'],
+      female: true
+    }
+  },
+  {
+    name: "Caelus - The Harmony",
+    img: "caelus3.png",
+    opts: {
+      faction: ['ast'],
+      star: ['5'],
+      ct: ['imaginary'],
+      path: ['th'],
+      male: true
+    }
+  },
+  {
+    name: "Yunli",
+    img: "yunli.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['physical'],
+      path: ['td'],
+      female: true,
+      upcoming: true
+    }
+  },
+  {
+    name: "Jiaoqiu",
+    img: "jiaoqiu.png",
+    opts: {
+      faction: ['xl'],
+      star: ['5'],
+      ct: ['fire'],
+      path: ['tn'],
+      male: true,
+      upcoming: true
+    }
+  },
+]
